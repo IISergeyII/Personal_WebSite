16db7225f002787c1d7fc3296e78c021837f4b83
IISergeyII <31198738+IISergeyII@users.noreply.github.com>
2021-11-23 14:03:44 +0300

    Add Skills and Scrolling JS

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// Opening of the relevant Skillset tab and closing all other Skillset tabs.
+function openSkill(skillName) {
+    let i, tabcontent, tablinks;
+
+    // Delete previously opened
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    document.getElementById(skillName).style.display = "block";
+}
+
+function closeSkill(elem) {
+    elem.parentElement.parentElement.style.display='none';
+}
+
+window.addEventListener('scroll', function(e) {
+    let navigationHeight = $("#navmenu").outerHeight();
+    let scrollDistance = $(window).scrollTop();
+
+    $(".navsection").each(function (i) {
+        if ($(this).position().top <= scrollDistance + navigationHeight) {
+            $(".navbar-nav").find("li.active").removeClass("active");
+            $(".navbar-nav li").eq(i).addClass("active");
+        }
+    });
+});
+
+
+window.onload = function () {
+    var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
+    var footer = document.getElementsByClassName('footer-time')[0];
+    footer.innerHTML += '<p>Page load time is ' + loadTime + ' seconds</p>';
+
+}
